5d70392d21a4742e16ec4acc550ad8500f23d6c6
Kevin C McKenna <kevmckenna@gmail.com>
2017-09-04 22:48:09 -0700

    added HTML forms, more CSS, comments and functions for JS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 console.log('it loaded...')
 
+// Enter player names
+
+var $playerName = []
+
+var $theForm = $('.form-bar')
+$theForm.on('submit', function(event){
+    event.preventDefault()
+    if ($playerName.val() === ''){
+        confirm("Enter your Name!")
+    } else { 
+
+    }
+})
+
+// Start game
+
 var newGame = $('#play-game')
 newGame.on('click', function() {
     newGame.remove('#play-game')
@@ -18,9 +34,36 @@ newGame.on('click', function() {
                 borderRadius: Math.random() * 10,
             }, 750)
         }, 750)
-        $('body').on('click', 'new-fly', function(){
-            $(this).remove().fadeOut('slow', function(){
-                $('body').append(newGame)
-            })
-    })
+    $(theFly).click(function() {
+        console.log('fly has been swatted')
+        p1Score.innerHTML += scoreP1;
+        $(this).remove();
+        })
+    if (theFly === '') {    
+    $('body').append(newGame.button)
+    } 
 })
+
+// Timer for the game
+
+var timeUp = setTimeout(theTimer, 30000);
+
+    function theTimer() {
+        switchPlayer();
+    } 
+    
+    clearTimeout(timeUp)
+
+// Declare winner of the game
+
+var winner 
+
+if (p1Score.innerHTML > p2Score.innerHTML){
+    prompt('Player One is the Winner!')
+if (p2Score.innerHTML > p1Score.innerHTML){ 
+    prompt('Player Two is the Winner!');
+}
+
+p1Score.innerHTML += score1;
+
+// Restart game
