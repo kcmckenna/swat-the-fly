ea3da5484cd241d0330d0765ff6f27983744985c
Kevin C McKenna <kevmckenna@gmail.com>
2017-09-06 12:21:16 -0700

    talked to Jimmy, fixed scoreboard and image issues, fly respawns too much

--- js/main.js
+++ js/main.js
@@ -14,10 +14,10 @@ var switchPlayer = p2
 
 function getNames() {
     p1.name = document.getElementById('p1-name').value;
-    document.getElementById('p1-score-text').innerText = p1.name + ":" + 0;
+    document.getElementById('player-one-name').innerText = p1.name + ": ";
     console.log(p1.name)
     p2.name = document.getElementById('p2-name').value;
-    document.getElementById('p2-score-text').innerText = p2.name + ":" + 0;
+    document.getElementById('player-two-name').innerText = p2.name + ": ";
     console.log(p2.name)
 }
 
@@ -36,7 +36,7 @@ function getNames() {
             getNames();
             console.log('Names Added')
             $('.form-bar').fadeOut(1000);
-            $('#play-game').fadeIn(1000);
+            $('#play-game').fadeIn(3000);
         }
     })
 // $('.form-bar').hide(); // Remove this line to show the form
@@ -52,7 +52,7 @@ newGame.on('click', function() {
     // credit to soundbible.com for audio (http://soundbible.com/396-Fly-Buzzing.html)
     flyBuzzing.loop = true
     flyBuzzing.play()
-    var counter = 30
+    var counter = 5
     var timer = document.querySelector('.timer')
     var theIntervalId = setInterval(fn, 1000)
     function fn(){
@@ -75,25 +75,24 @@ newGame.on('click', function() {
         }
     }
     // Make Fly Appear
-    var theFly = $('<div>').addClass('new-fly custom').innerText('FLY')
+    var theFly = $('<img src="images/fly.gif">') //$('<div>').addClass('new-fly').text('FLY')
     // Source for fly image (https://atomic8497.deviantart.com/art/Fly-651283182)
-    theFly.css ({
-        left: Math.random() * 550,
-        top: Math.random() * 550,
-    })
-    $("body").append(theFly)
+    // theFly.css ({
+    //     left: Math.random() * 550,
+    //     top: Math.random() * 550,
+    // })
+    $("#game-container").append(theFly)
     function showFly() {
         theFly.css ({
             left: Math.random() * 550,
             top: Math.random() * 550,
         })
-        $("body").append(theFly) 
         theFly.fadeIn(1000)
         setInterval(function (){
-            // theFly.animate ({
-            //     left: Math.random() * 550,
-            //     top: Math.random() * 550,
-            // }, 1000)
+            theFly.animate ({
+                left: Math.random() * 550,
+                top: Math.random() * 550,
+            }, 1000)
         }, 1000)
     } 
     showFly();
@@ -106,7 +105,7 @@ newGame.on('click', function() {
         console.log(currentPlayer.score)
         currentPlayer.score = currentPlayer.score + 1;
         console.log(currentPlayer.score)
-        currentPlayer.board = currentPlayer.score // <---- Need to fix this
+        currentPlayer.board.text(currentPlayer.score) // <---- FIXED!
         console.log('score added to player scoreboard')
         $(this).hide();
         showFly();
