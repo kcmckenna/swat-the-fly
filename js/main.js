744ac96b52b7e9bc1ccd5e1cf160bd2ade484fd6
Kevin C McKenna <kevmckenna@gmail.com>
2017-09-06 16:29:45 -0700

    JS still being debugged for timer, declaring winner and reset

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Enter player names
 
-var p1 = {score: 0, name: '', board: $('#player-one-score')}
-var p2 = {score: 0, name: '', board: $('#player-two-score')}
+var p1 = {score: 0, name: '', board: $('#player-one-score'), takeTurn: false}
+var p2 = {score: 0, name: '', board: $('#player-two-score'), takeTurn: false}
 // var players = {p1, p2}
 var currentPlayer = p1 
 var switchPlayer = p2
@@ -19,22 +19,22 @@ function getNames() {
 
 // Input form used to enter names
 
-    var $theForm = $('form')
-    $theForm.on('submit', function(event){
-        if (document.getElementById('p1-name').value === '') {
-            confirm('Enter your Name!')
-            console.log('No Names Added')
-        } if (document.getElementById('p2-name').value === '') {
-            confirm('Enter your Name!')
-            console.log('No Names Added')
-        } else { 
-            event.preventDefault();
-            getNames();
-            console.log('Names Added')
-            $('.form-bar').fadeOut(1000);
-            $('#play-game').fadeIn(1000);
-        }
-    })
+var $theForm = $('form')
+$theForm.on('submit', function(event){
+    if (document.getElementById('p1-name').value === '') {
+        confirm('Enter your Name!')
+        console.log('No Names Added')
+    } if (document.getElementById('p2-name').value === '') {
+        confirm('Enter your Name!')
+        console.log('No Names Added')
+    } else { 
+        event.preventDefault();
+        getNames();
+        console.log('Names Added')
+        $('.form-bar').fadeOut('fast');
+        $('#play-game').fadeIn(2700);
+    }
+})
 
 // $('.form-bar').hide(); // Remove this line to show the form
 
@@ -42,13 +42,14 @@ function getNames() {
 
 var newGame = $('#play-game')
 newGame.on('click', function() {
-    // Remove Start Button
+    // Hide Start Button
     newGame.hide('#play-game')
     // Timer Starts
     var flyBuzzing = new Audio ('sounds/fly_buzzing.wav')
     // credit to soundbible.com for audio (http://soundbible.com/396-Fly-Buzzing.html)
     flyBuzzing.loop = true
     flyBuzzing.play()
+    currentPlayer.takeTurn = true;
     var counter = 10
     var timer = document.querySelector('.timer')
     var theIntervalId = setInterval(fn, 1000)
@@ -59,35 +60,36 @@ newGame.on('click', function() {
             console.log('Time is Done')
             flyBuzzing.pause()
             theFly.hide('fast');
-            newGame.show('#play-game')
             currentPlayer = p2
+            newGame.show('#play-game').text(currentPlayer.name + "'s Turn")
             // if p1 and p2 have both clicked, check score and announce winner
-                // if ([p1.score >= 0] && [p2.score > 0])
-                //     findWinner();
-                // }
         } else {
             counter = (counter - 1)
             timer.innerHTML = "Timer: " + counter
             console.log('Time is Ticking')
         }
     }
-    
+                // if (p2.takeTurn = true) {
+                //     findWinner();
+                // }
+
     // Make Fly Appear
 
     var theFly = $('<img src="images/fly.gif">')
     // Source for fly image (https://atomic8497.deviantart.com/art/Fly-651283182)
     $("#game-container").append(theFly)
     function showFly() {
+        // $(this).show();
         theFly.css ({
             left: Math.random() * 550,
             top: Math.random() * 550,
         })
         theFly.fadeIn(1000)
         setInterval(function (){
-            theFly.animate ({
-                left: Math.random() * 550,
-                top: Math.random() * 550,
-            }, 1500)
+            // theFly.animate ({
+            //     left: Math.random() * 550,
+            //     top: Math.random() * 550,
+            // }, 1500)
         }, 1500)
     } 
     showFly();
@@ -107,40 +109,42 @@ newGame.on('click', function() {
         $(this).hide();
             if (counter != 0) {
                 showFly();
+                console.log('new fly appeared')
             }
-        console.log('new fly appeared')
-        })
+    })  
 
+    // Declare winner of the game
+
+    var winner = ''
+    function findWinner() {
+        if ((p1.score > p2.score) && (p2.takeTurn = true)) {
+            winner = p1.name;
+            alert (p1.name + " is the Winner!");
+            resetGame(); // <--- Not Defined Yet
+        } else if (((p1.score > 0) == (p2.score > 0)) && (p2.takeTurn = true)) {
+            prompt ("It's a Tie! Play again!");
+            resetGame();
+        } else { 
+            winner = p2.name;
+            alert (p2.name + " is the Winner!");
+            resetGame();
+        var winnerFound = $('<div>').addClass('winner').text('WINNER')
+        $("#game-container").append(winner).text(winner + "is the Winner!")
+        winner.fadeIn(1000)
+        }
+    }
 })
 
 // var timeUp = setTimeout(theTimer, 30000);
 //     function theTimer() {
 //         switchPlayer();
 
-// Declare winner of the game
-
-// var winner = ''
-// function findWinner() {
-//     if (p1.score > p2.score){
-//         alert (p1.name + " is the Winner!")
-            // resetGame();
-//      if (p2.score > p1.score){ 
-//         alert (p2.name + " is the Winner!")
-            // resetGame();
-//      } else {
-//         prompt ("It's a Tie! Play again!")
-            // resetGame();
-//      }
-//      var winner = $('<div>').addClass('winner').text('WINNER')
-//      $("body").append(winner)
-//      winner.fadeIn(2000)
-// }
-
 // Restart game
 
 // function resetGame () {
-//     newGame.show('#play-game')
-//     $theForm (":reset")
+//     newGame.show('#play-game').text("Play Again?!")
+//     $theForm = ''
+//     theFly = ''
 //     p1 = ''
 //     p2 = '' 
 // }
